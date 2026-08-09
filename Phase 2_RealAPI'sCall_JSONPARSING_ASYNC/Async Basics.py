25b56194f5d58db485d981c6fe1d301a94f0c5c0
Rupesh <rupeshghimire377@gmail.com>
2026-08-09 23:34:24 +0545

    async concepts

--- Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Async Basics.py
+++ Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Async Basics.py
@@ -60,7 +60,7 @@ async def handle_multiple_users():
             call_llm_async(session, "What's the meaning of life?", "user_4"),
             call_llm_async(session, "What's the weather in Paris?", "user_5"),
             call_llm_async(session, "what's the weather today?", "user_6"),
-            ca
+            call_llm_async(session, "What's the weather today?", "user_7"),
         ]
 
         # Run all tasks at the same time and wait for ALL to complete
