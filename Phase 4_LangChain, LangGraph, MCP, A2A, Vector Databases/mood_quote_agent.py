12b5c8298ef0fffafa7fb18fac3942fd8fba9966
Rupesh <rupeshghimire377@gmail.com>
2026-08-31 22:42:52 +0545

    improving

--- Phase 4_LangChain, LangGraph, MCP, A2A, Vector Databases/mood_quote_agent.py
+++ Phase 4_LangChain, LangGraph, MCP, A2A, Vector Databases/mood_quote_agent.py
@@ -19,6 +19,7 @@ class MoodQuoteAgent:
             "Happiness is not by chance, but by choice. — William George Jordan",
             "Keep your face always toward the sunshine—and shadows will fall behind you. — Walt Whitman",
             "The best way to cheer yourself is to try to cheer someone else up. — Mark Twain",
+            "Every day may not be good, but there is something good in every day. — Unknown",
             "Happiness is the medicine of the life"
         ],
         "sad": [
