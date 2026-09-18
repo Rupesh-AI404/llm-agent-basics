ba65a98eccff0d9cfcdb9d8572dc0e6151580ca6
Rupesh <rupeshghimire377@gmail.com>
2026-09-18 12:36:12 +0545

    added headers

--- Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Async Basics.py
+++ Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Async Basics.py
@@ -23,7 +23,8 @@ async def call_llm_async(
         "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
         "Content-Type": "application/json",
         "data-encoding": "utf-8",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "Accept-Language": "en-US",
     }
 
     payload = {
