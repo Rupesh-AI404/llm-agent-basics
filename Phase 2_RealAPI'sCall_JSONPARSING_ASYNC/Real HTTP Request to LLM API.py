c619cee24a80e3b37ea57026b0f08ff236ae9f00
Rupesh <rupeshghimire377@gmail.com>
2026-09-25 22:58:53 +0545

    added payload: max_tokens

--- Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Real HTTP Request to LLM API.py
+++ Phase 2_RealAPI'sCall_JSONPARSING_ASYNC/Real HTTP Request to LLM API.py
@@ -45,6 +45,7 @@ def call_openai(prompt: str) -> str:
             {"role": "user", "content": "What is the capital of the United States?"},
             {"role": "assistant", "content": "Washington, D.C."},
             {"role": "assistant", "content": "Tokyo, D.C."},
+            {"role": "assistant", "content": "Japan, D.C."},
 
 
 
